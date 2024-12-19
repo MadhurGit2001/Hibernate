d0792f6cebdbbf671f06b1628a628f0a0601e4ab
MadhurGit2001 <madhurkant60@gmail.com>
2024-12-19 17:10:42 +0530

    Third commit

--- HibernateProject/src/main/java/com/tut/HibernateProject/App.java
+++ HibernateProject/src/main/java/com/tut/HibernateProject/App.java
@@ -1,13 +1,46 @@
 package com.tut.HibernateProject;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		// Build the SessionFactory
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg2.xml").buildSessionFactory();
+
+		// Create a new Student object
+		Student st = new Student();
+		st.setStudentId(1);
+		st.setName("Madhur");
+		st.setRollNo(190);
+		st.setAddress("E-Block Sector 20 Noida");
+
+		// Open a new session
+		Session session = sessionFactory.openSession();
+
+		// Start a transaction
+		Transaction transaction = session.beginTransaction();
+
+		// Save the student object
+		session.save(st);
+
+		// Commit the transaction
+		transaction.commit();
+
+		// Close the session
+		session.close();
+
+		// Close the SessionFactory
+		sessionFactory.close();
+
+		System.out.println("Student saved successfully!");
+	}
 }
---
+++ HibernateProject/src/main/java/com/tut/HibernateProject/Student.java
@@ -0,0 +1,60 @@
+package com.tut.HibernateProject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_mpstrak_student")
+public class Student {
+	@Id
+	private Integer studentId;
+	private String name;
+	private String address;
+	private Integer rollNo;
+
+	public Student(Integer studentId, String name, String address, Integer rollNo) {
+		super();
+		this.studentId = studentId;
+		this.name = name;
+		this.address = address;
+		this.rollNo = rollNo;
+	}
+
+	public Student() {
+		super();
+	}
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(Integer rollNo) {
+		this.rollNo = rollNo;
+	}
+
+}
